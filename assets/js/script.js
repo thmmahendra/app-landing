8cac022344ca927b199a0a1b5d29cd03ab815dc7
thmmahendra <thmmahendra@gmail.com>
2023-07-17 22:14:38 +0545

    Initial Commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,49 @@
 'use strict';
 
-// add event on element
+/** 
+ * add event on element
+* */
 
 const addEventOnElem = function (elem, type, callback) {
-
+    if (elem.length > 1) {
+        for (let i = 0; i < elem.length; i++) {
+            elem[i].addEventListener(type, callback);
+        }
+    } else {
+        elem.addEventListener(type, callback);
+    }
 }
+
+/** 
+ * nav toggle
+* */
+
+const navbar = document.querySelector("[data-navbar]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+
+const toggleNavbar = function () {
+    navbar.classList.toggle("active");
+}
+
+addEventOnElem(navToggler, "click", toggleNavbar);
+
+const closeNavbar = function () {
+    navbar.classList.remove("active");
+}
+
+addEventOnElem(navLinks, "click", closeNavbar);
+
+/** 
+ * header active
+* */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 100) {
+        header.classList.add("active");
+    } else {
+        header.classList.remove("active");
+    }
+})

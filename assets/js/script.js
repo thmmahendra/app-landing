66690788fcc5f34787a7d7bc230b42032f2a2d33
thmmahendra <thmmahendra@gmail.com>
2023-07-22 19:27:44 +0545

    Initial Commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,11 +39,14 @@ addEventOnElem(navLinks, "click", closeNavbar);
 * */
 
 const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
 
 window.addEventListener("scroll", function () {
     if (window.scrollY > 100) {
         header.classList.add("active");
+        backTopBtn.classList.add("active");
     } else {
         header.classList.remove("active");
+        backTopBtn.classList.remove("active");
     }
-})
+});
